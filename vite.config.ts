83dd1521ce1eaebb1b6b5b5bc7d2bc11ed0005a3
Shaun Savage <savages@savages.com>
2024-12-10 17:54:36 +0000

    changed logo, added ttop-level-await so it will build

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,15 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import wasm from "vite-plugin-wasm";
+import topLevelAwait from "vite-plugin-top-level-await";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: 
     [
+      wasm(),
+      topLevelAwait(),
       vue(),
-      wasm()
     ],
   server: {
     host: '0.0.0.0'
